15e4b87782a5ab1b287e452b294c0a9e80e6d296
aswanth8111 <aswanthkumarvk@gmail.com>
2023-11-27 20:34:27 +0530

    third process

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Carousel from 'react-bootstrap/Carousel';
-import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
@@ -24,7 +23,7 @@ function Hero() {
               </div>
             </Col>
             <Col xs={12} lg={7}>
-              <img src="https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/Highlight-Slim-Multi-Canvas-M365-Anthem-Attract-Image-Create:VP2-859x540" alt="" />
+              <img className='im' src="https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/Highlight-Slim-Multi-Canvas-M365-Anthem-Attract-Image-Create:VP2-859x540" alt="" />
             </Col>
           </Row>
         </div>
@@ -34,7 +33,7 @@ function Hero() {
       <Carousel.Item interval={500}>
         <div>
           <Row className=' g-0'>
-            <Col  ><img className='' src="https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/Highlight-Surface-Pro-9-M1-Family-02-1:VP2-859x540" alt="" /></Col>
+            <Col  ><img className='im' src="https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/Highlight-Surface-Pro-9-M1-Family-02-1:VP2-859x540" alt="" /></Col>
             <Col  style={{ backgroundColor: 'rgba(242,242,242,255)' }} className='caro2 ' >
               <h1 style={{ textDecoration: 'none' }} className='ps-3'>Surface pro 9</h1>
               <p className='ps-3'>Tablet versatility and laptop power — all in one ultra-portable device</p>
